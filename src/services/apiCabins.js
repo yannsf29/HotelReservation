b1a27e5ea59fb0140672e52a4cfec911c344a181
yann crenn <yanncrenn@yanns-Air.lan>
2024-05-21 14:46:27 -0700

    implemented mutation

---
+++ src/services/apiCabins.js
@@ -0,0 +1,23 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins couod not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabin could not be deleted");
+  }
+
+  return data;
+}
