79f3a4b5d5781c99bf5cbea47d50731275850f45
yann crenn <yanncrenn@yanns-Air.lan>
2024-05-27 10:45:08 -0700

    added the upload of images

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ydelovvqqcyncppbgqnc.supabase.co";
+export const supabaseUrl = "https://ydelovvqqcyncppbgqnc.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZWxvdnZxcWN5bmNwcGJncW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyMTk1MjUsImV4cCI6MjAzMTc5NTUyNX0.DHiuA1uCaA4TB3yAooT3zXJn0-ft0TpoVkXCVuFz9mI";
 const supabase = createClient(supabaseUrl, supabaseKey);
